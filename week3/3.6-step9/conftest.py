a9c10f0ea61a3fc4f21e3d96b1206c38e8457f27
Mariia Rumiantceva <mari.s70c3@gmail.com>
2020-03-25 23:54:44 +0300

    task 9 fixed - added default language

--- week3/3.6-step9/conftest.py
+++ week3/3.6-step9/conftest.py
@@ -4,13 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru, en, es...")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     options = Options()
+    print(request.config)
     options.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption("language")})
     print("\nstart chrome browser for test..")
     browser = webdriver.Chrome(options=options)
